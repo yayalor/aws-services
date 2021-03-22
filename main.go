5d0a53f63bbe758f432a91886c37b727f79f7d7b
kis9a <kis9ax@gmail.com>
2021-03-22 19:27:56 +0900

    feat: get path

--- main.go
+++ main.go
@@ -3,27 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	doc, err := goquery.NewDocument("https://gobyexample.com/")
-	checkErr(err)
-	doc.Find("ul a").Each(func(_ int, s *goquery.Selection) {
-		path, _ := s.Attr("href")
-		con, err := goquery.NewDocument("https://gobyexample.com/" + path)
-		checkErr(err)
-		fmt.Println(con)
+	supportedLanguages := []string{"ar", "cn", "de", "en", "es", "fr", "id", "it", "jp", "ko", "pt", "ru", "th", "tr", "tw", "vi"}
+	fmt.Println(supportedLanguages)
 
-		con.Find("ul a").Each(func(_ int, s *goquery.Selection) {
-		})
+	lang := os.Args[1]
+	baseUrl := "https://aws.amazon.com/"
+	fmt.Println(baseUrl, lang)
+	doc, err := goquery.NewDocument(baseUrl + lang)
+	checkError(err)
+
+	items := doc.Find(".lb-content-item")
+	items.Each(func(_ int, item *goquery.Selection) {
+		path, _ := item.Find("a").Attr("href")
+		fmt.Println(path)
 	})
 }
 
-func checkErr(err error) {
+func checkError(err error) {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func typeof(i interface{}) interface{} {
+	return reflect.TypeOf(i)
 }
